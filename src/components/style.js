ba570cbb6d5e373e7ae3069c968598dfe1a03b37
Lucas Choi <Lucas.choi@kwitech.com>
2025-12-01 16:37:43 -0800

    Layout

---
+++ src/components/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const S = {};
+S.Button = styled.button`
+  background-color: rgb(236, 236, 236);
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 18px;
+  white-space: nowrap;
+`;
+
+export default S;
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+  return <div>Page NotFound</div>;
+};
+
+export default Notfound;
